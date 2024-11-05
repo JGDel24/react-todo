84269734101ef065c566900114ccfb41423db62e
Joel <fitness.joeld@gmail.com>
2024-11-05 13:30:30 -0500

    week 2 forum added

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './TodoList'
+import TodoList from './TodoList'
+import AddTodoForm from './AddTodoForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const todoList = [
-    {id: 1, title: 'cook steak'},
-    {id: 2, title: 'clean car'},
-    {id: 3, title: 'shop'}
-  ];
-
   return (
     <>
-        <h1>To Do List</h1>
-        <ul>
-         {todoList.map(toDo =>
-          <li key={toDo.id}>{toDo.title}</li>
-         )}
-        </ul>
+      <h1>To Do List</h1>
+      <AddTodoForm />
+      <TodoList />
+
     </>
   )
 }
