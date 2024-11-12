2650a6d28fa1b0be4f63245162aa805b6f941162
Joel <fitness.joeld@gmail.com>
2024-11-12 18:34:09 -0500

    Typo and unessasary code removed

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AddTodoForm() {
     return (
         <form>
-            <label htmlFor="todoTitl">Title</label>
+            <label htmlFor="todoTitle">Title</label>
             <input type="text" id="todoTitle" />
             <button type="submit">Add</button>
 
--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
