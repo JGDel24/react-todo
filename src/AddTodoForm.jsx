6a1be5776186f61d089592a476d24f11c0b196a8
Joel <fitness.joeld@gmail.com>
2024-11-22 14:11:46 -0500

    lifting state and props handle

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
-function AddTodoForm(props) {
+function AddTodoForm({onAddTodo}) {
+
+    const [todoTitle,  setTodoTitle] = useState('');
+
+    function handleTitleChange(event) {
+        const newTodoTitle = event.target.value;
+
+        setTodoTitle(newTodoTitle);
+    }
 
     function handleAddTodo(event) {
         event.preventDefault();
-        const todoTitle = event.target.title.value;
+        
         console.log(todoTitle);
-        props.onAddTodo(todoTitle)
-        event.target.title.value = "";
+        onAddTodo({title: todoTitle, id: Date.now()})
+        setTodoTitle('');
 
 
     }
@@ -15,7 +24,10 @@ function AddTodoForm(props) {
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input type="text" id="todoTitle" name='title'/>
+            <input type="text" id="todoTitle" name='title' 
+            value={todoTitle}
+            onChange={handleTitleChange}
+            />
             <button type="submit">Add</button>
 
         </form>
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function TodoListItem(props) {
+function TodoListItem({toDo}) {
     return (
-        <li>{props.toDo.title}</li>
+        <li>{toDo.title}</li>
     )
 
 }
