a91ea4563d4ce82cc681931d2d4d55ae63383e0b
Joel <fitness.joeld@gmail.com>
2024-12-02 13:24:17 -0500

    hooks and fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import './TodoList'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
-function App() {
-  
-  const [todoList, setTodoList] = useState([]);
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodos = localStorage.getItem('savedTodoList');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
 
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+
+  }, [todoList]);
+
+  return [todoList, setTodoList]
+}
+
+function App() {
+
+  const [todoList, setTodoList] = useSemiPersistentState();
+ 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
