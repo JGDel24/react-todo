a5a0963f35f89623b8e70845537d5dccbe1cc4fb
Joel <fitness.joeld@gmail.com>
2024-12-19 13:20:19 -0500

    Asynchronous Data Conditional Rendering Advanced and Impossible State

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,45 @@ import './TodoList'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(() => {
-    const savedTodos = localStorage.getItem('savedTodoList');
-    return savedTodos ? JSON.parse(savedTodos) : [];
-  });
 
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-
-  }, [todoList]);
-
-  return [todoList, setTodoList]
-}
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTodos = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data:{
+            todoList: [
+              {id: 1, title: 'Walk the dog'},
+              {id: 2, title: 'Do laundry'}
+            ]
+          }
+        })
+      }, 2000);
+    })
+
+    fetchTodos.then(response => {
+      setTodoList(response.data.todoList)
+      setIsLoading(false);
+    })
+
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]) 
 
   function removeTodo(id) {
     const newTodoList = todoList.filter(todo => todo.id !== id);
     setTodoList(newTodoList);
   }
 
-  const [todoList, setTodoList] = useSemiPersistentState();
- 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
@@ -36,8 +52,14 @@ function App() {
     <>
       <h1>To Do List</h1>
       <AddTodoForm onAddTodo={addTodo}/>
+        {isLoading ? (
+          <p>Loading...</p>) : (
+            <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+          )
+        }
       
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      
+      
 
     </>
   )
